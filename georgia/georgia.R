3afa2c3f21710f1cca39c07b0895d0c350c7a85e
mcclainclain <matthew.mcclain08@icloud.com>
2023-11-29 19:24:29 -0600

    start of presentation work

--- georgia/georgia.R
+++
@@ -1,9 +0,0 @@
-library(RSelenium)
-library(tidyverse)
-
-driver <- rsDriver(browser = "firefox",
-                         chromever = "113.0.5672.63") # alt: chromever = NULL
-remote_driver <- driver[["client"]]
-remote_driver$open()
-
-remote_driver$navigate("https://www.latlong.net/convert-address-to-lat-long.html")
